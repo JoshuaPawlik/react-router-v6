2f1b00211091001923292cf62b582f933b162c40
Joshua Pawlik <joshuapawlik12@gmail.com>
2022-08-04 16:35:43 -0500

    Add Dynamic Routes

--- src/pages/Book.js
+++ src/pages/Book.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export function Book() {
-  return <div>Book</div>
+  const { id } = useParams()
+  return <div>Book {id} </div>
 }
 
--- src/pages/BookList.js
+++ src/pages/BookList.js
@@ -1,5 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export function BookList() {
-  return <div>BookList</div>
+  return (
+    <>
+      <h1>BookList</h1>
+      <Link to='/books/1'>Book 1</Link>
+      <Link to='/books/2'>Book 2</Link> 
+    </>
+  )
 }
